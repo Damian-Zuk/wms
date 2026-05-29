e0d2a6fef182deb65de35bead43a63368129ea25
Damian Żuk <55585560+Damian-Zuk@users.noreply.github.com>
2026-05-29 22:37:05 +0200

    Setup Client.Vue project (wms-client)

---
+++ Client.Vue/wms-client/src/main.ts
@@ -0,0 +1,30 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/main.css'
+import 'primeicons/primeicons.css'
+
+import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueQueryPlugin)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.dark',
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+    },
+  },
+})
+
+app.mount('#app')
---
+++ Client.Vue/wms-client/src/router/index.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    { path: '/', name: 'dashboard', component: () => import('@/features/dashboard/Dashboard.vue') },
+  ],
+})
+
+export default router
